d81c0835c1a13c0347e8e4a88f4ecf9b2ccd364b
Michael Skogberg <michael.skogberg@futurice.com>
2023-01-07 17:23:45 +0200

    Up deps; use kts

---
+++ automotive/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.skogberglabs.polestar"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.skogberglabs.polestar"
+        minSdk = 29
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+val autoVersion = "1.2.0"
+
+dependencies {
+    implementation("androidx.car.app:app:$autoVersion")
+    implementation("androidx.car.app:app-automotive:$autoVersion")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("androidx.car.app:app-testing:1.3.0-rc1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
