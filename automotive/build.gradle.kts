b9a8f4cd699b0335dc17444b30b760528b4f0b91
Michael Skogberg <michael.skogberg@futurice.com>
2023-01-07 17:59:14 +0200

    Add GA

--- automotive/build.gradle.kts
+++ automotive/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.5.1")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("androidx.car.app:app-testing:1.3.0-rc1")
+    testImplementation("androidx.car.app:app-testing:1.2.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
