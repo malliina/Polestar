11e088aef9e0c898141bf3ffee4c7c865c177945
Michael Skogberg <michael.skogberg@futurice.com>
2023-04-01 18:09:13 +0300

    Up version

--- automotive/build.gradle.kts
+++ automotive/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.skogberglabs.polestar"
         minSdk = 30 // Android 11
         targetSdk = 33
-        versionCode = 9
-        versionName = "1.8"
+        versionCode = 10
+        versionName = "1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
